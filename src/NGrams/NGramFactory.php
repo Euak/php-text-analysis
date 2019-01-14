a0f2fe53e289d53be51ff55d4d5afed74f124fd7
Kaue Oliveira Almeida <ueak21@gmail.com>
2019-01-14 16:07:47 -0200

    Type hinted NGrams
    Type hinted NGrams methods.

--- src/NGrams/NGramFactory.php
+++ src/NGrams/NGramFactory.php
@@ -11,19 +11,19 @@ class NGramFactory
 {
     const BIGRAM = 2;
     const TRIGRAM = 3;
-    
+
     /**
      * Protect the constructor
      */
     protected function __construct(){}
-    
+
     /**
      * Generate Ngrams from the tokens
      * @param array $tokens
      * @param int $nGramSize
      * @return  array return an array of the ngrams
      */
-    static public function create(array $tokens, $nGramSize = self::BIGRAM, $separator = ' ') : array
+    static public function create(array $tokens, int $nGramSize = self::BIGRAM, $separator = ' ') : array
     {
         $separatorLength = strlen($separator);
         $length = count($tokens) - $nGramSize + 1;
@@ -31,12 +31,12 @@ class NGramFactory
             return [];
         }
         $ngrams = array_fill(0, $length, ''); // initialize the array
-        
+
         for($index = 0; $index < $length; $index++)
         {
             for($jindex = 0; $jindex < $nGramSize; $jindex++)
             {
-                $ngrams[$index] .= $tokens[$index + $jindex]; 
+                $ngrams[$index] .= $tokens[$index + $jindex];
                 if($jindex < $nGramSize - $separatorLength) {
                     $ngrams[$index] .= $separator;
                 }
