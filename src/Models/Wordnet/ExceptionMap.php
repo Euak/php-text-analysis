d61551d26bedb4df47557af3cc2b8af2f1952827
Kaue Oliveira Almeida <ueak21@gmail.com>
2019-01-14 16:07:05 -0200

    Type hinted Models
    Type hinted Models methods.

--- src/Models/Wordnet/ExceptionMap.php
+++ src/Models/Wordnet/ExceptionMap.php
@@ -6,57 +6,57 @@ namespace TextAnalysis\Models\Wordnet;
  * Maps the exception file data into an object
  * @author dcardin
  */
-class ExceptionMap 
+class ExceptionMap
 {
     /**
      *
-     * @var string pos 
+     * @var string pos
      */
     protected $pos = null;
-    
+
     /**
      *
      * @var string[] String of exception words to look for
      */
     protected $exceptionList = [];
-    
+
     /**
      *
      * @var string The target word the strings in the exceptionList get mapped to
      */
     protected $target = null;
-    
+
     /**
-     * 
+     *
      * @param string $pos
      * @param string $target
      * @param array $exceptionList
      */
-    public function __construct($pos, $target, array $exceptionList) 
+    public function __construct(string $pos, string $target, array $exceptionList) 
     {
         $this->pos = $pos;
         $this->target = $target;
         $this->exceptionList = $exceptionList;
     }
-    
+
     /**
-     * 
+     *
      * @return string
      */
     public function getPos()
     {
         return $this->pos;
     }
-    
+
     /**
-     * 
+     *
      * @return string
      */
     public function getTarget()
     {
         return $this->target;
     }
-    
+
     /**
      * @return string[]
      */
@@ -64,5 +64,5 @@ class ExceptionMap
     {
         return $this->exceptionList;
     }
-    
+
 }
--- src/Models/Wordnet/Lemma.php
+++ src/Models/Wordnet/Lemma.php
@@ -6,47 +6,47 @@ namespace TextAnalysis\Models\Wordnet;
  * Data model to capture wordnet's lemma ie data loads from the index.* files
  * @author dcardin
  */
-class Lemma 
+class Lemma
 {
     use \TextAnalysis\Traits\WordnetPointerSymbolMap;
-    
+
     /**
      * @string lower case ASCII text of word or collocation. Collocations are formed by joining individual words with an underscore (_ ) character.
     */
     protected $word;
-    
-    
+
+
     /**
      *
-     * @var int Number of synsets that lemma is in. This is the number of senses 
-     * of the word in WordNet. See Sense Numbers below for a discussion of how 
+     * @var int Number of synsets that lemma is in. This is the number of senses
+     * of the word in WordNet. See Sense Numbers below for a discussion of how
      * sense numbers are assigned and the order of synset_offset s in the index files.
      */
     protected $synsetCnt = 0;
-    
+
     /**
      *
      * @var int Number of different pointers that lemma has in all synsets containing it.
      */
     protected $pCnt = 0;
 
-    
+
     /**
-     * @var int Byte offset in data.pos file of a synset containing lemma . Each 
-     * synset_offset in the list corresponds to a different sense of lemma in 
+     * @var int Byte offset in data.pos file of a synset containing lemma . Each
+     * synset_offset in the list corresponds to a different sense of lemma in
      * WordNet. synset_offset is an 8 digit, zero-filled decimal integer t
      * hat can be used with fseek(3) to read a synset from the data file
      */
     protected $synsetOffsets = [];
-    
+
     /**
      *
      * @var Synset[]
      */
     protected $synsets = [];
-    
+
     /**
-     * 
+     *
      * @param string $word
      * @param string $pos
      * @param int $synsetCnt
@@ -54,63 +54,63 @@ class Lemma
      * @param array $ptrSymbols
      * @param array $synsetOffsets
      */
-    public function __construct($word, $pos, $synsetCnt, $pCnt, array $ptrSymbols, array $synsetOffsets) 
+    public function __construct(string $word, string $pos, int $synsetCnt, int $pCnt, array $ptrSymbols, array $synsetOffsets) 
     {
         $this->word = $word;
         $this->pos = $pos;
         $this->synsetCnt = $synsetCnt;
         $this->pCnt = $pCnt;
-        $this->ptrSymbols = $ptrSymbols;        
+        $this->ptrSymbols = $ptrSymbols;
         $this->synsetOffsets = $synsetOffsets;
     }
-    
+
     /**
-     * 
+     *
      * @return string
      */
     public function getWord()
     {
         return $this->word;
     }
-    
 
-    
+
+
     /**
-     * 
+     *
      * @return int
      */
     public function getSynsetCnt()
     {
         return $this->synsetCnt;
     }
-    
+
     /**
-     * 
+     *
      * @return int
      */
     public function getPCnt()
     {
         return $this->pCnt;
     }
-    
+
     /**
-     * 
+     *
      * @return int[]
      */
     public function getSynsetOffsets()
     {
         return $this->synsetOffsets;
     }
-    
+
     /**
-     * 
+     *
      * @return string[]
      */
     public function getPtrSymbols()
     {
         return $this->ptrSymbols;
     }
-    
+
     /**
      * Get the synsets for this lemma
      * @return Synset[]
@@ -119,9 +119,9 @@ class Lemma
     {
         return $this->synsets;
     }
-    
+
     /**
-     * 
+     *
      * @param Synset[] $synsets
      * @return \TextAnalysis\Models\Wordnet\Lemma
      */
@@ -130,5 +130,5 @@ class Lemma
         $this->synsets = $synsets;
         return $this;
     }
-    
+
 }
